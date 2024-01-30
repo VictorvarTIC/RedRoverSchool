1ab358bb9aad725cd31a9a499aa635993865cc18
victorvartic <vartic.victor@gmail.com>
2024-01-30 13:16:28 +0200

    HW2 execution

---
+++ src/HW2.java
@@ -0,0 +1,34 @@
+public class HW2 {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 3;
+
+        System.out.println("сложение");
+        System.out.println(a + b);
+        System.out.println();
+
+        System.out.println("умножение");
+        System.out.println(a * b);
+        System.out.println();
+
+        System.out.println("вычитание");
+        System.out.println(a - b);
+        System.out.println();
+
+        System.out.println("деление");
+        System.out.println(a / b);
+        System.out.println();
+
+        System.out.println("остаток от деления");
+        System.out.println(a % b);
+        System.out.println();
+
+        System.out.println("проверка на четность а");
+        System.out.println(a % 2);
+        System.out.println();
+
+        System.out.println("проверка на четность b");
+        System.out.println(b % 2);
+    }
+}
