786f4ace2bc1bb9811df14ccb1035619ae2a2f09
victorvartic <vartic.victor@gmail.com>
2024-01-30 11:27:33 +0200

    HW1 execution

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -2,5 +2,7 @@ public class Lesson1 {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.print("My name is Victor! ");
+        System.out.println("This is first lesson)");
     }
 }
