90a3e9e2d7df0b221712e6d47746178db4d40abf
victorvartic <vartic.victor@gmail.com>
2024-02-25 18:10:35 +0200

    2

---
+++ src/HW6_2.java
@@ -0,0 +1,37 @@
+//https://www.youtube.com/watch?v=AY2-hhtn4mo&list=PL6Gtav8N4O7hBE01SU-rFPuQ2qg_a7IwQ&index=18
+public class HW6_2 {
+//        Задача №1
+//        Необходимо написать 4 метода:
+//        сложение 2х чисел
+//        вычитание 2х чисел
+//        умножение 2х чисел
+//        деление 2х чисел
+    public static double getSum(double a, double b) {
+        return a + b;
+    }
+
+    public static double getMinus(double a, double b) {
+        return a - b;
+    }
+
+    public static double getDev(double a, double b) {
+        return a / b;
+    }
+
+    public static double getMulti(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getSum(20,80));
+        System.out.println(getMinus(20,80));
+        System.out.println(getDev(20,80));
+        System.out.println(getMulti(20,80));
+    }
+}
+//https://www.codewars.com/kata/53ee5429ba190077850011d4/train/java
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/java
+//https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/java
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/java
+//https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/java
